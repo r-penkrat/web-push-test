b851b7dcb90abb2233be5198ffe3a25323ab0e3c
Ruslan Penkrat <r.penkrat@setplex.com>
2021-05-19 18:53:10 +0300

    Initial commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+// This is simple push receiver service worker.
+// Example not used because it doesn't allow customization (setBackgroundEventHandler)
+// https://github.com/xakep666/push-article/
+self.addEventListener("push", event => { event.waitUntil(onPush(event)) });
+
+async function onPush(event) {
+    const push = event.data.json();
+    console.log("push received", push)
+
+    const { notification = {}, data = {} } = {...push};
+
+    await self.registration.showNotification(notification.title, {
+        body: notification.body,
+    })
+
+    if (data.id) {
+        await fetch(`${self.location.origin}/api/v1/notifications/${data.id}/confirm`, { method: "POST" })
+    }
+}
